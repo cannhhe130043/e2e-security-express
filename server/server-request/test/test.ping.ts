e6dadbaeeb623978c68d57e0e907f22f2bc5801b
cannh <cannhhe130043@gmail.com>
2020-06-20 12:33:42 +0700

    Add test ping interval

---
+++ server/server-request/test/test.ping.ts
@@ -0,0 +1,13 @@
+import fetch from 'node-fetch'
+
+const url = 'http://localhost:3000/getData'
+
+const ping = () => {
+    setInterval(async () => {
+        const response = await fetch(url, { method: 'GET' })
+        const data = await response.json()
+        console.log(data)       
+    }, 3000)
+}
+
+ping()
