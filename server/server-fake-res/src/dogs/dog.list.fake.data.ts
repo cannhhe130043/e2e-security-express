9344611ed9cd857e1249303346d32237743e2c5a
cannh <cannhhe130043@gmail.com>
2020-06-20 13:55:01 +0700

    add feature reject data

---
+++ server/server-fake-res/src/dogs/dog.list.fake.data.ts
@@ -0,0 +1,39 @@
+import { Dog, DogType } from "./dog.entity";
+
+export const Dogs: Dog[] = [
+    {
+        id: 1,
+        name: 'Dog 1 fake',
+        type: DogType.Chihuahua
+    },
+    {
+        id: 2,
+        name: 'Dog 2 fake',
+        type: DogType.Pitbull
+    },
+    {
+        id: 3,
+        name: 'Dog 3 fake',
+        type: DogType.Shiba
+    },
+    {
+        id: 4,
+        name: 'Dog 4 fake',
+        type: DogType.Chihuahua
+    },
+    {
+        id: 5,
+        name: 'Dog 5 fake',
+        type: DogType.Shiba
+    },
+    {
+        id: 6,
+        name: 'Dog 6 fake',
+        type: DogType.Pitbull
+    },
+    {
+        id: 7,
+        name: 'Dog 7 fake',
+        type: DogType.Shiba
+    }
+]
--- server/server-request/test/test.ping.ts
+++ server/server-request/test/test.ping.ts
@@ -5,6 +5,10 @@ const url = 'http://localhost:3000/getData'
 const ping = () => {
     setInterval(async () => {
         const response = await fetch(url, { method: 'GET' })
+        if(response.status !== 200){
+            console.log('Reject data')
+            return
+        }
         const data = await response.json()
         console.log(data)
     }, 3000)
